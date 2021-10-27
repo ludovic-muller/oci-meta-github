f92e1269cb08f290a23ef7682a1ba9aa6f03c2f8
Ludovic Muller <ludovicmuller1@gmail.com>
2021-10-28 00:08:36 +0200

    add support for multiple images

--- src/main.rs
+++ src/main.rs
@@ -29,36 +29,55 @@ fn get_tag(github_ref: &str) -> Option<String> {
 }
 
 fn main() -> anyhow::Result<()> {
+    // should be passed as environment variables
     let default_branch = "main";
     let github_ref = "refs/heads/feature-branch-1";
     let github_run_id = "42";
     let github_sha = "abcdefghijklmnopqrstuvwxyz";
-    let prefix = "example.com/org/image:";
+    let images = "example.com/org/image,example.com/org/image2";
 
+    // some computed values
     let sha = short_sha(github_sha);
     let branch = get_branch(github_ref);
     let tag = get_tag(github_ref);
-    let v_prefix = format!("{}v", prefix);
-
     let is_latest = match branch {
         Some(b) => b == default_branch,
         None => false,
     };
 
-    let mut tags = format!("{}pipeline-{},{}git-{}", prefix, github_run_id, prefix, sha);
-    if is_latest {
-        tags.push_str(&format!(",{}latest", prefix));
-    }
+    // results will be stored there
+    let mut tags = String::from("");
 
-    if let Some(tag) = tag {
-        if let Ok(version) = Semver::from_str(&tag) {
-            let version_tags = version.single_line(v_prefix);
-            if !version_tags.is_empty() {
-                tags.push_str(&format!(",{}", version_tags));
+    // loop over all images
+    let images = images.split(',');
+    for image in images {
+        let image = image.trim().to_lowercase();
+        let prefix = format!("{}:", image);
+        let v_prefix = format!("{}v", prefix);
+
+        if !tags.is_empty() {
+            tags.push(',');
+        }
+
+        tags.push_str(&format!(
+            "{}pipeline-{},{}git-{}",
+            prefix, github_run_id, prefix, sha
+        ));
+        if is_latest {
+            tags.push_str(&format!(",{}latest", prefix));
+        }
+
+        if let Some(ref tag) = tag {
+            if let Ok(version) = Semver::from_str(tag) {
+                let version_tags = version.single_line(v_prefix);
+                if !version_tags.is_empty() {
+                    tags.push_str(&format!(",{}", version_tags));
+                }
             }
         }
     }
 
+    // print output
     github_print("tags", tags);
 
     Ok(())
