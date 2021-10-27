3966e7ebcf18df1cce4406a2f092d6cbc87b34d0
Ludovic Muller <ludovicmuller1@gmail.com>
2021-10-27 23:28:00 +0200

    short sha support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{cmp, str::FromStr};
 
 use semver::Semver;
 
@@ -6,15 +6,33 @@ fn github_print(output_name: &str, value: String) {
     println!("::set-output name={}::{}", output_name, value);
 }
 
+fn short_sha(sha: &str) -> &str {
+    let sha_len = sha.chars().count();
+    let sha_len = cmp::min(sha_len, 8);
+
+    let mut end: usize = 0;
+    sha.chars()
+        .into_iter()
+        .take(sha_len)
+        .for_each(|x| end += x.len_utf8());
+
+    &sha[..end]
+}
+
 fn main() -> anyhow::Result<()> {
-    let pipeline = "42";
-    let sha = "12345678";
+    let default_branch = "main";
+    let github_ref = "refs/heads/feature-branch-1";
+    let github_run_id = "42";
+    let github_sha = "abcdefghijklmnopqrstuvwxyz";
+
+    let sha = short_sha(github_sha);
+
     let is_latest = true;
     let version = Semver::from_str("v1.2.3")?;
     let prefix = "example.com/org/image:";
     let v_prefix = format!("{}v", prefix);
     let mut tags = version.single_line(v_prefix);
-    let ci_tags = format!("{}pipeline-{},{}git-{}", prefix, pipeline, prefix, sha);
+    let ci_tags = format!("{}pipeline-{},{}git-{}", prefix, github_run_id, prefix, sha);
     if !tags.is_empty() {
         tags.push(',');
     }
